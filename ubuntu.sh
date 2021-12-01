484bc26dd4b26b9837f6b270bc95b22135d1980e
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-01 20:36:07 +0900

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -50,7 +50,7 @@ fi
 mkdir -p $dir
 cd $dir
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Decompressing the ubuntu rootfs, please wait...\n"
-tar -zxf "${dir}" --exclude='dev'||:
+tar -zxf "${base}" --exclude='dev' -C "${dir}" ||:
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m The ubuntu rootfs have been successfully decompressed!\n"
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Fixing the resolv.conf, so that you have access to the internet\n"
 printf "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > etc/resolv.conf
