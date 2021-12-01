6850c1ff34377d3e13f333e610b6089316cd0ac7
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-01 21:13:36 +0900

    avoid tzdata asking

--- ubuntu.sh
+++ ubuntu.sh
@@ -141,6 +141,8 @@ fi
 unwanted="tumbler ubuntu-report popularity-contest apport whoopsie apport-symptoms snap snapd apparmor synaptic rsyslog man-db yelp-xsl yelp"
 wanted="htop ncdu nano vim bash-completion wget curl ffmpeg p7zip-full p7zip-rar python3-pip python3-requests python3-numpy python3-matplotlib python3-pandas python3-sklearn python3-pyftpdlib python3-bs4 unar pv aria2"
 (
+##disable tzdata asking
+echo -e "DEBIAN_FRONTEND=noninteractive"
 echo -e "apt-get update \napt-get purge -y --auto-remove $unwanted \napt-mark hold $unwanted \napt-get install -y $wanted"
 echo -e "cd /root \nmkdir -p bin \ncd bin \ncurl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp &&\n
 chmod a+rx yt-dlp"
